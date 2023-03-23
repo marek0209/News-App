07e63a8ea61bd4727f4d50b94d6d91a14ce8d9b3
marek0209 <marek.urbaniec1@gmail.com>
2023-03-23 20:02:47 +0100

    Add ViewSwitcher component

--- ngNews/src/components/Header/Header.tsx
+++ ngNews/src/components/Header/Header.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ViewSwitcher from "./ViewSwitcher";
 
 const Header = () => {
   return (
     <>
-      <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5  dark:bg-gray-900">
+      <nav className="bg-white border-gray-800 px-2 sm:px-4 py-2.5  dark:bg-gray-800">
         <div className="container flex flex-wrap items-center justify-between mx-auto">
           <Link to="/" className="flex items-center">
             <img
@@ -17,6 +18,7 @@ const Header = () => {
             </span>
           </Link>
           <div className="flex md:order-2">
+            <ViewSwitcher />
             <button
               type="button"
               className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
---
+++ ngNews/src/components/Header/ViewSwitcher.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+const ViewSwitcher = () => {
+  return (
+    <>
+      <div className="mr-10 bg-gray-800 text-sm text-gray-500 leading-none  inline-flex">
+        <button
+          className="inline-flex items-center transition-colors duration-300 ease-in focus:outline-none hover:text-blue-500 focus:text-blue-500 rounded-l-full px-4 py-2 active"
+          id="grid"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className="fill-current w-4 h-4 mr-2"
+          >
+            <rect x="3" y="3" width="7" height="7"></rect>
+            <rect x="14" y="3" width="7" height="7"></rect>
+            <rect x="14" y="14" width="7" height="7"></rect>
+            <rect x="3" y="14" width="7" height="7"></rect>
+          </svg>
+          <span>Grid</span>
+        </button>
+        <button
+          className="inline-flex items-center transition-colors duration-300 ease-in focus:outline-none hover:text-blue-500 focus:text-blue-500 rounded-r-full px-4 py-2"
+          id="list"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className="fill-current w-4 h-4 mr-2"
+          >
+            <line x1="8" y1="6" x2="21" y2="6"></line>
+            <line x1="8" y1="12" x2="21" y2="12"></line>
+            <line x1="8" y1="18" x2="21" y2="18"></line>
+            <line x1="3" y1="6" x2="3.01" y2="6"></line>
+            <line x1="3" y1="12" x2="3.01" y2="12"></line>
+            <line x1="3" y1="18" x2="3.01" y2="18"></line>
+          </svg>
+          <span>List</span>
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default ViewSwitcher;
