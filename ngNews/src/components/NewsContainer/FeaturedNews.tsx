58924e08887919e97768a5f839c75c7006dfcbe7
marek0209 <marek.urbaniec1@gmail.com>
2023-03-24 00:04:06 +0100

    Add FeaturedNews component

---
+++ ngNews/src/components/NewsContainer/FeaturedNews.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { News } from "../../types/newsTypes";
+import { removeSource } from "../../Utils/sourceRemove";
+import { formatDate } from "../../Utils/dateFormatter";
+
+interface Props {
+  news: News;
+}
+const FeaturedNews: React.FC<Props> = ({ news }) => {
+  return (
+    <>
+      <div className="flex flex-col items-center sm:px-5 md:flex-row">
+        <div className="flex flex-col items-start justify-center w-full h-full pt-6 pr-0 pb-6 pl-0 mb-6 md:mb-0 md:w-1/2">
+          <div
+            className="flex flex-col items-start justify-center h-full space-y-3 transform md:pr-10 lg:pr-16
+            md:space-y-5"
+          >
+            <div
+              className="bg-green-500 flex items-center leading-none rounded-full text-gray-50 pt-1.5 pr-3 pb-1.5 pl-2
+              uppercase inline-block"
+            >
+              <p className="inline">
+                <svg
+                  className="w-3.5 h-3.5 mr-1"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0
+                  00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755
+                  1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1
+                  0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+                  />
+                </svg>
+              </p>
+              <p className="inline text-xs font-medium">New</p>
+            </div>
+            <a className="text-4xl font-bold leading-none lg:text-5xl xl:text-6xl">
+              {removeSource(news.title, news.author ?? "")}
+            </a>
+            <div className="pt-2 pr-0 pb-0 pl-0">
+              <p className="text-sm font-medium inline">Source:</p>
+              <a className="inline text-sm font-medium mt-0 mr-1 mb-0 ml-1 underline">
+                {news.author}
+              </a>
+              <p className="inline text-sm font-medium mt-0 mr-1 mb-0 ml-1">
+                · {formatDate(news.publishedAt)} ·
+              </p>
+              <p className="text-gray-200 text-sm font-medium inline mt-0 mr-1 mb-0 ml-1">
+                1hr 20min. read
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="w-full md:w-1/2">
+          <div className="block">
+            <img
+              src={
+                news.urlToImage
+                  ? news.urlToImage
+                  : "https://images.unsplash.com/photo-1495020689067-958852a7765e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80"
+              }
+              className="object-cover rounded-lg max-h-64 sm:max-h-96 btn- w-full h-full"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default FeaturedNews;
--- ngNews/src/components/NewsContainer/NewsContainer.tsx
+++ ngNews/src/components/NewsContainer/NewsContainer.tsx
@@ -7,6 +7,7 @@ import { News } from "../../types/newsTypes";
 import { removeSource } from "../../Utils/sourceRemove";
 
 import { formatDate } from "../../Utils/dateFormatter";
+import FeaturedNews from "./FeaturedNews";
 
 const NewsContainer = () => {
   const dispatch = useDispatch();
@@ -31,14 +32,24 @@ const NewsContainer = () => {
 
   return (
     <>
-      {news.map((n: News) => (
-        <div key={n.url}>
-          <h1>{removeSource(n.title, n.author)}</h1>
-          {n.author}
-          {formatDate(n.publishedAt)}
-          <a href={n.url}>Read more</a>
+      <div className="text-gray-900 pt-12 pr-0 pb-14 pl-0 bg-white">
+        <div
+          className="w-full pt-4 pr-5 pb-6 pl-5 mt-0 mr-auto mb-0 ml-auto space-y-5 sm:py-8 md:py-12 sm:space-y-8 md:space-y-16
+      max-w-7xl"
+        >
+          {news.length > 0 && <FeaturedNews news={news[0]} />}
+          <div class="grid grid-cols-12 sm:px-5 gap-x-8 gap-y-16">
+            {news.slice(1).map((n: News) => (
+              <div key={n.url}>
+                <h1>{removeSource(n.title, n.author ?? "")}</h1>
+                {n.author}
+                {formatDate(n.publishedAt)}
+                <a href={n.url}>Read more</a>
+              </div>
+            ))}
+          </div>
         </div>
-      ))}
+      </div>
     </>
   );
 };
