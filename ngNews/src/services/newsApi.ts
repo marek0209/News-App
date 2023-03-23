d0d028a1cbe04e74e2d382ba765bbced04b8757b
marek0209 <marek.urbaniec1@gmail.com>
2023-03-23 22:50:10 +0100

    Fix .env import of api key in service

--- ngNews/src/services/newsApi.ts
+++ ngNews/src/services/newsApi.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosResponse } from "axios";
 import { News } from "../types/newsTypes";
 
-const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = import.meta.env.VITE_APP_API_URL;
 
 interface NewsApiResponse {
   articles: News[];
@@ -9,7 +9,6 @@ interface NewsApiResponse {
 
 export const getNews = async (country: string): Promise<News[]> => {
   const url = `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${API_KEY}`;
-
   const response: AxiosResponse<NewsApiResponse> = await axios.get(url);
 
   return response.data.articles;
